670c0882892231a4319fd835313be8f5e662e380
chennuganesh7316 <163857497+chennuganesh7316@users.noreply.github.com>
2024-03-25 17:53:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const app=exp()
 app.get("/hello",async (req,res)=>{
     res.send({status:true,message:"Got a scuss"})
 })
-app.listen(8080,()=>{
+app.listen(80,()=>{
     console.log(`server is running on port 8080`)
 })
