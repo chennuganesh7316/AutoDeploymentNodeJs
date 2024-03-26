2cb43d93b04d5e61048e2cb7683b3bd6caa54a5d
chennuganesh7316 <163857497+chennuganesh7316@users.noreply.github.com>
2024-03-26 17:28:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app=exp()
 app.get("/TestHello",async (req,res)=>{
     res.send({status:true,message:"I have done autoDeployment using github actions in aws ec2"})
 })
-app.post("/createUser",async (req,res=>{
+app.post("/createUser",async (req,res)=>{
     res.send({status:true,message:"user created scussfully"})
 })
 app.listen(80,()=>{
