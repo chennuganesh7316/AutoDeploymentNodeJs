68535eebee0bfb02b1a4e9628433033d0c3a510b
chennuganesh7316 <163857497+chennuganesh7316@users.noreply.github.com>
2024-03-26 15:25:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const exp=require("express")
 const app=exp()
 app.get("/data",async (req,res)=>{
-    res.send({status:true,message:"Got a scuss for message"})
+    res.send({status:true,message:"hello this is my new automatic deployment with ec2"})
 })
 app.listen(80,()=>{
     console.log(`server is running on port 8080`)
