8af033833274b376dc0fd7d3c1e349f441471b2c
chennuganesh7316 <163857497+chennuganesh7316@users.noreply.github.com>
2024-03-26 17:25:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app=exp()
 app.get("/TestHello",async (req,res)=>{
     res.send({status:true,message:"I have done autoDeployment using github actions in aws ec2"})
 })
+app.post("/createUser",async (req,res=>{
+    res.send({status:true,message:"user created scussfully"})
+})
 app.listen(80,()=>{
     console.log(`server is running on port 8080`)
 })
