5b7851c99b0d191bc7e0996eabbcb0f3116e1d65
chennuganesh7316 <163857497+chennuganesh7316@users.noreply.github.com>
2024-03-26 15:23:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const exp=require("express")
 const app=exp()
 app.get("/data",async (req,res)=>{
-    res.send({status:true,message:"Got a scuss"})
+    res.send({status:true,message:"Got a scuss for message"})
 })
 app.listen(80,()=>{
     console.log(`server is running on port 8080`)
