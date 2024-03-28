4527f20eb1259abcb2dd71baac39f8773cf75754
chennuganesh7316 <163857497+chennuganesh7316@users.noreply.github.com>
2024-03-28 17:39:08 +0530

    Update app.js

--- service1/app.js
+++ service1/app.js
@@ -1,10 +1,10 @@
 const exp=require("express")
 const app=exp()
 app.get("/TestHello",async (req,res)=>{
-    res.send({status:true,message:"service hjsgjdshgjhgjhghjsd"})
+    res.send({status:true,message:"code 1 backend services implemented"})
 })
 app.post("/createUser",async (req,res)=>{
-    res.send({status:true,message:"service is running on auto"})
+    res.send({status:true,message:"code 2 backend services implemented"})
 })
 app.listen(8080,()=>{
     console.log(`server is running on port 8080`)
