f95763b780572f199bdbce42a0894b39db60727e
chennuganesh7316 <163857497+chennuganesh7316@users.noreply.github.com>
2024-04-04 14:56:09 +0530

    Update app.js

--- service1/app.js
+++ service1/app.js
@@ -4,7 +4,7 @@ app.get("/TestHello",async (req,res)=>{
     res.send({status:true,message:"this is dev"})
 })
 app.post("/createUser",async (req,res)=>{
-    res.send({status:true,message:"code 2 backend services implemented"})
+    res.send({status:true,message:"code 2 backend services implemented for"})
 })
 app.listen(80,()=>{
     console.log(`server is  running on port 8080`)
