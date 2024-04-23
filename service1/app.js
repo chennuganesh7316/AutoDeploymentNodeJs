4b79c56059b31a9bf54dc8e0a251b01bb73edac7
chennuganesh7316 <163857497+chennuganesh7316@users.noreply.github.com>
2024-04-23 16:45:41 +0530

    Update app.js

--- service1/app.js
+++ service1/app.js
@@ -3,7 +3,7 @@ const app=exp()
 app.get("/TestHello",async (req,res)=>{
     res.send({status:true,message:"backend code is implemented using main branch"})
 })
-app.post("/createUser",async (req,res)=>{
+app.post("/createUserhg,async (req,res)=>{
     res.send({status:true,message:"code 2 backend services implemented"})
 })
 app.listen(80,()=>{
